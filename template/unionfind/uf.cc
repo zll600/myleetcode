0ffe65b6101812e9be27388f1628cbe36e018c0b
zll600 <3400692417@qq.com>
2022-04-25 22:40:29 +0800

    add union find implementation

---
+++ template/unionfind/uf.cc
@@ -0,0 +1,37 @@
+#include <vector>
+
+using namespace std;
+
+class UF {
+ public:
+    UF(int len) : data_(len), cnt_(len) {
+        for (int i = 0; i < len; ++i) {
+            data_[i] = i;
+        }
+    }
+
+    int Find(int x) {
+        while (x != data_[x]) {
+            data_[x] = data_[data_[x]];
+            x = data_[x];
+        }
+
+        return x;
+    }
+
+    void Union(int x, int y) {
+        int root_x = Find(x);
+        int root_y = Find(y);
+        
+        data_[root_x] = data_[root_y];
+        --cnt_;
+    }
+
+    int TotalCount() const {
+        return cnt_;
+    }
+
+ private:
+    vector<int> data_;
+    int cnt_;
+};
