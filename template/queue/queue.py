8cb67a3500b5c360ae05621a8056d7a6dcba53c8
zll600 <3400692417@qq.com>
2022-05-22 07:34:57 +0800

    add template que with python

---
+++ template/queue/queue.py
@@ -0,0 +1,28 @@
+class OurQueue:
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+    
+    def __len__(self):
+        return len(self.in_stack) + len(self.out_stack)
+
+    def push(self, obj):
+        self.in_stack.append(obj)
+
+    def pop(self):
+        if not self.out_stack:
+            self.out_stack = self.in_stack[::-1]
+            self.in_stack = []
+
+        return self.out_stack.pop();
+
+if __name__=="__main__":
+    que = OurQueue()
+    que.push(1)
+    que.push(2)
+    que.push(3)
+
+    print(f'{que.pop()}')
+    print(f'{que.pop()}')
+    print(f'{que.pop()}')
+
